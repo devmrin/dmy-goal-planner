d15774d2aba121055c8bb9cfb5708150e9a09ac4
Mrinmay <devmrin@gmail.com>
2019-04-19 17:19:33 +0530

    base styles - d + m + y

--- src/screens/Monthly/Monthly.jsx
+++ src/screens/Monthly/Monthly.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from "react";
 import { Base } from "layout";
+import { ForwardArrow } from "assets";
 
 import "./Monthly.scss";
 
 class Monthly extends Component {
+  state = {
+    monthlyGoalsArray: [11, 12, 13, 14, 15],
+    cfMonthlyGoalsArray: [3, 5, 9]
+  };
+
   render() {
-    return <Base />;
+    let { monthlyGoalsArray, cfMonthlyGoalsArray } = this.state;
+    return (
+      <Base>
+        <div className="monthly__goals__wrapper">
+          <div className="monthly__goals__cf__wrapper">
+            <div className="monthly__goals__cf__header">
+              <img
+                src={ForwardArrow}
+                alt="forward"
+                className="monthly__goals__forwaded__img"
+              />
+              Carry Forwarded
+            </div>
+            {cfMonthlyGoalsArray.map(goalObj => (
+              <div className="monthly__cf__goal__container" key={goalObj}>
+                <div className="monthly__cf__goal__radio">
+                  <div className="monthly__cf__goal__active" />
+                </div>
+                <div className={"monthly__cf__goal__text"}>
+                  {`Complete Trigonometry Lesson ${goalObj}`}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="monthly__goals__regular__wrapper">
+            {monthlyGoalsArray.map((goalObj, i) => (
+              <div className="monthly__goal__container" key={goalObj}>
+                <div className="monthly__goal__radio">
+                  <div className="monthly__goal__active" />
+                </div>
+                <div className="monthly__goal__text">
+                  {`Complete Trigonometry Lesson ${goalObj}`}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Base>
+    );
   }
 }
 
--- src/screens/Yearly/Yearly.jsx
+++ src/screens/Yearly/Yearly.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from "react";
 import { Base } from "layout";
+import { ForwardArrow } from "assets";
 
 import "./Yearly.scss";
 
 class Yearly extends Component {
+  state = {
+    yearlyGoalsArray: [11, 12, 13, 14, 15],
+    cfYearlyGoalsArray: [3, 5, 9]
+  };
+
   render() {
-    return <Base />;
+    let { yearlyGoalsArray, cfYearlyGoalsArray } = this.state;
+    return (
+      <Base>
+        <div className="yearly__goals__wrapper">
+          <div className="yearly__goals__cf__wrapper">
+            <div className="yearly__goals__cf__header">
+              <img
+                src={ForwardArrow}
+                alt="forward"
+                className="yearly__goals__forwaded__img"
+              />
+              Carry Forwarded
+            </div>
+            {cfYearlyGoalsArray.map(goalObj => (
+              <div className="yearly__cf__goal__container" key={goalObj}>
+                <div className="yearly__cf__goal__radio">
+                  <div className="yearly__cf__goal__active" />
+                </div>
+                <div className={"yearly__cf__goal__text"}>
+                  {`Complete Trigonometry Lesson ${goalObj}`}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="yearly__goals__regular__wrapper">
+            {yearlyGoalsArray.map((goalObj, i) => (
+              <div className="yearly__goal__container" key={goalObj}>
+                <div className="yearly__goal__radio">
+                  <div className="yearly__goal__active" />
+                </div>
+                <div className="yearly__goal__text">
+                  {`Complete Trigonometry Lesson ${goalObj}`}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Base>
+    );
   }
 }
 
